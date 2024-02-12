fb2e2a7985be1894962c3435137a16ef89544aee
kaylakautai <kaylak106@hotmail.co.nz>
2024-02-13 12:13:30 +1300

    feat: Add weather API

---
+++ app/src/main/java/com/example/weatherforecastapp/network/WeatherApi.kt
@@ -0,0 +1,27 @@
+package com.example.weatherforecastapp.network
+
+import com.example.weatherforecastapp.model.Location
+import com.example.weatherforecastapp.model.Weather
+import com.example.weatherforecastapp.utils.Constants
+import retrofit2.http.GET
+import retrofit2.http.Query
+import javax.inject.Singleton
+
+@Singleton
+interface WeatherApi {
+    @GET(value = "geo/1.0/direct")
+    suspend fun getLocationFromCity(
+        @Query("q") query: String,
+        @Query("limit") limit: String = Constants.DEFAULT_CITY_LIMITS,
+        @Query("appid") appid: String = Constants.API_KEY
+    ): Location
+
+    @GET(value = "data/2.5/forecast/daily")
+    suspend fun getDailyForecast(
+        @Query("lat") lat: String,
+        @Query("long") long: String,
+        @Query("cnt") cnt: String = Constants.DEFAULT_DAYS,
+        @Query("units") units: String = Constants.DEFAULT_UNITS,
+        @Query("appid") appid: String = Constants.API_KEY
+    ): Weather
+}
---
+++ app/src/main/java/com/example/weatherforecastapp/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.weatherforecastapp.utils
+
+object Constants {
+    const val BASE_URL = "https://api.openweathermap.org/"
+    const val API_KEY = "b35594e90d2d29a24357777155540ccf"
+
+    const val DEFAULT_UNITS = "metric"
+    const val DEFAULT_DAYS = "7"
+    const val DEFAULT_CITY_LIMITS = "5"
+}
